87723b838f766b6c008925301c0441daf8c25a45
zarina777 <131705393+zarina777@users.noreply.github.com>
2025-04-23 15:56:27 +0500

    Update index.jsx

--- src/routes/LandingPage/Location/index.jsx
+++ src/routes/LandingPage/Location/index.jsx
@@ -13,7 +13,7 @@ const Location = () => {
           <span className="text-4">Местоположение</span>
         </Heading>
         <div>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d5994.635377077853!2d69.306275!3d41.301952!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDHCsDE4JzA3LjAiTiA2OcKwMTgnMjIuNiJF!5e0!3m2!1sru!2s!4v1745404749182!5m2!1sru!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d5994.635377077853!2d69.306275!3d41.301952!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDHCsDE4JzA3LjAiTiA2OcKwMTgnMjIuNiJF!5e0!3m2!1sru!2s!4v1745404749182!5m2!1sru!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
         </div>
         <div className="flex flex-col gap-4">
           <p>Tashkent, Узбекистан</p>
